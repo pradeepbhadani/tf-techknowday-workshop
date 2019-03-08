e9b10489013ad4dd33455402f15fb0d7d4a35b28
Pradeep Bhadani <pradeep.bhadani@gmail.com>
2019-03-08 09:05:40 +0000

    Add demo1 code

---
+++ demo1/main.tf
@@ -0,0 +1,13 @@
+# Terraform AWS provider
+# https://www.terraform.io/docs/providers/aws/
+provider "aws" {
+  profile = "demo"
+  region  = "us-west-2"
+}
+
+# Create AWS EC2 instance
+resource "aws_instance" "my_instance" {
+  ami           = "ami-095cd038eef3e5074"
+  instance_type = "t2.micro"
+  subnet_id     = "my-subnet-id-123" # Update this value with your subnet-id
+}
